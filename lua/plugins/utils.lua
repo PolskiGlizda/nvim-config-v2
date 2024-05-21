43dbe25444e2bea8145d63f3657b8a46e3e62e6c
Filip Gliszczyński <filipgliszczynski@gmail.com>
2024-05-21 11:57:58 +0200

    add folke/todo-comments.nvim

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,7 +1,11 @@
 return {
-    "folke/which-key.nvim",
-    -- "numToStr/Comment.nvim",
-    "NvChad/nvim-colorizer.lua",
-    "altermo/ultimate-autopair.nvim",
-    "fladson/vim-kitty",
+	"folke/which-key.nvim",
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	-- "numToStr/Comment.nvim",
+	"NvChad/nvim-colorizer.lua",
+	"altermo/ultimate-autopair.nvim",
+	"fladson/vim-kitty",
 }
