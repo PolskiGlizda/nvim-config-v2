415e9051bf80ae70f6ca9b7fb92da32def6e7fbc
Filip Gliszczyński <filipgliszczynski@gmail.com>
2025-01-04 15:50:53 +0100

    replace nvim-cmp with blink.cmp

--- lua/config/lazy/lsp.lua
+++ lua/config/lazy/lsp.lua
@@ -3,33 +3,35 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
+        "saghen/blink.cmp",
+        "rafamadriz/friendly-snippets",
         "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
-        "zbirenbaum/copilot-cmp",
         "zbirenbaum/copilot.lua",
     },
-
-    config = function()
+    opts = {
+        servers = {
+            lua_ls = {},
+            clangd = {},
+            pyright = {},
+            ts_ls = {},
+            rust_analyzer = {},
+            gopls = {},
+            bashls = {},
+            emmet_language_server = {},
+            html = {},
+            htmx = {},
+            cssls = {},
+            hls = {},
+            tailwindcss = {},
+        }
+    },
+    config = function(_, opts)
         require("copilot").setup({
             suggestion = { enabled = false },
             panel = { enabled = false },
         })
-        require("copilot_cmp").setup()
-        local cmp = require("cmp")
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities()
-        )
-
+        -- require("copilot_cmp").setup()
         require("fidget").setup({})
         require("mason").setup()
         require("mason-lspconfig").setup({
@@ -45,74 +47,15 @@ return {
                 "html",
                 "htmx",
                 "cssls",
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end,
-
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                },
-                            },
-                        },
-                    })
-                end,
+                "hls",
+                "tailwindcss"
             },
         })
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-                ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" }, -- For luasnip users.
-            }, {
-                { name = "copilot" },
-                { name = "buffer" },
-                { name = "path" },
-                { name = "cmdline" },
-            }),
-        })
-
+        local lspconfig = require("lspconfig")
+        for servers, config in pairs(opts.servers) do
+            config.capabilities = require("blink.cmp").get_lsp_capabilities()
+            lspconfig[servers].setup(config)
+        end
         vim.diagnostic.config({
             -- update_in_insert = true,
             float = {
